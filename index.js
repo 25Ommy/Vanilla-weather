ab9eefc75d642fd983c9e7797775c4cf741257d7
25Ommy <156078030+25Ommy@users.noreply.github.com>
2024-01-13 00:51:14 -0800

    added an id

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-function searchFormSubmit(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let city = searchInputElement.value;
+  let apiKey = "8c8894fb74b2o09027cb643c5t33b24a";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = searchFormSubmit.value;
+
+  searchCity(searchInput.value);
 }
 let searchForElement = document.querySelector("#search-form");
-searchForElement.addEventListener("submit", searchFormSubmit);
+searchForElement.addEventListener("submit", searchSubmit);
